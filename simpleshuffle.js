48287bef5a78a56177cdc7fea8546b34010e5ca8
Josh Vogel <joshvogel6@comcast.net>
2013-05-03 16:10:54 -0700

    Added Distribution Table
    Created functions to create a distribution table of cards based on value
    and index position.

--- simpleshuffle.js
+++ simpleshuffle.js
@@ -1,3 +1,5 @@
+//Simple shuffle function. Takes in an array called "deck"
+//Returns deck with elements shifted randomly.
 function simpleShuffle (deck, deckSize)
 { 
     for(i=0;i<deckSize;i++){
@@ -10,7 +12,7 @@ function simpleShuffle (deck, deckSize)
     }
     return deck;
 }
-
+//Creates an ordered integer array of deckSize.
 function makeDeck (deckSize)
 {
     var deck = [];
@@ -20,10 +22,78 @@ function makeDeck (deckSize)
     }
     return deck;
 }
+//function for displaying the values in a 2D array.
+function display2DArray(multiArray, rows){
+	for(i=0;i<rows;i++){
+		console.log(multiArray[i]);
+		
+	}
+}
 
+function numberSpreadInitialize(rows, columns){
+	var multiArray =[];
+	
+	for (i=0;i<rows;i++){
+		multiArray[i] = [];
+		for(j=0;j<=columns;j++){
+		
+			if (j == 0){
+				multiArray[i][j] = i;
+			}
+			else
+				multiArray[i][j] = 0;
+		}
+	}
+	return multiArray;
+}
+
+function numberSpread(multiArray, deck, deckSize){
+	for (i=0;i<deckSize;i++){
+		for(j=0;j<=deckSize;j++){
+			if(deck[i] == j){
+				multiArray[i][j] = multiArray[i][j] + 1;
+			}
+		}
+	}
+	
+	return multiArray;
+}
+
+function shuffleDistro(multiArray, deck, deckSize){
+	var numberOfShuffles = 10000;
+	var shuffledDeck = deck;
+	var tempArray = multiArray;
+	for(k=0;k<numberOfShuffles;k++){
+		shuffledDeck = simpleShuffle(shuffledDeck, deckSize);
+		tempArray = numberSpread(tempArray, shuffledDeck, deckSize);
+		
+	
+		
+	}
+	
+	
+	return tempArray;
+}
+	
+
+
+//making and verifying making a deck array.
 var deckSize = 10;
 var deck =  makeDeck(deckSize);
+console.log('Beginning Deck');
 console.log(deck);
 
+//shuffling and verifying shuffling array.
 deck = simpleShuffle(deck, deckSize);
+console.log('Example of Shuffled Deck.');
 console.log(deck);
+
+//making integer array for number spread.
+var rows = deckSize;
+var columns = deckSize;
+var numberSpreadTable = numberSpreadInitialize(rows, columns);
+numberSpreadTable = shuffleDistro(numberSpreadTable, deck, deckSize);
+console.log('Distribution of cards after each shuffle over 10000 shuffles.');
+display2DArray(numberSpreadTable, rows);
+
+
